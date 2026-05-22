377478f28b4ea346c8e98f52e0310163153e8672
Ghessica Santos <ghessicas@gmail.com>
2026-05-22 01:04:53 -0300

    Feat: add inicializar no Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import java.sql.SQLException;
+import view.LoginScreen;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
-
+    public static void main(String[] args) {
+        LoginScreen.main(args);
     }
 }
